52b17ae982af579ac5ab80843362e8d780750f92
刘毅 <liuyi2293@outlook.com>
2018-11-23 20:23:13 +0800

    Remove the resolution solution

--- src/config.h
+++ src/config.h
@@ -1,17 +1,9 @@
 ﻿#ifndef CONFIG_H
 #define CONFIG_H
 
-#if (1)
-
 #include <QDebug>
 
 #define VA_DEBUG_P qDebug()
 #define VA_DEBUG(e) VA_DEBUG_P << #e << "=" << e
-
-#endif
-
-extern float gScaledSize;
-
-#define FIT(x) int((x) * gScaledSize)
 
 #endif /* CONFIG_H */
