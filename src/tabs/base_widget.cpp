6f63b0ce71f771941503eb0dbd9517a8c738b971
刘毅 <liuyi2293@outlook.com>
2018-11-27 22:36:53 +0800

    Update the BaseWidget

--- src/tabs/base_widget.cpp
+++ src/tabs/base_widget.cpp
@@ -1,6 +1,22 @@
 ﻿#include "base_widget.h"
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 
 BaseWidget::BaseWidget(QWidget* parent) : QWidget(parent)
 {
+    helperWidget = new QWidget;
+    helperWidget->setFixedWidth(350);
+    view = new QGraphicsView;
+    settingsGroupBox = new QGroupBox(tr("Settings"));
+    descriptionGroupBox = new QGroupBox(tr("Description"));
+    QVBoxLayout* vLayout1 = new QVBoxLayout(helperWidget);
+    vLayout1->addWidget(settingsGroupBox);
+    vLayout1->addWidget(descriptionGroupBox);
+    vLayout1->setSpacing(10);
+    vLayout1->setMargin(0);
 
+    QHBoxLayout* hLayout1 = new QHBoxLayout(this);
+    hLayout1->addWidget(helperWidget);
+    hLayout1->addWidget(view);
+    hLayout1->setMargin(0);
 }
--- src/tabs/base_widget.h
+++ src/tabs/base_widget.h
@@ -2,12 +2,21 @@
 #define BASE_WIDGET_H
 
 #include "config.h"
-#include <QWidget>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGroupBox>
 
 class BaseWidget : public QWidget
 {
 public:
     BaseWidget(QWidget* parent = nullptr);
+
+public:
+    QWidget*        helperWidget;
+    QGraphicsView*  view;
+    QGraphicsScene* scene;
+    QGroupBox*      settingsGroupBox;
+    QGroupBox*      descriptionGroupBox;
 };
 
 #endif /* BASE_WIDGET_H */
