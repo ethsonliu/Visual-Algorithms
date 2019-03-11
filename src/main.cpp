6edeec7e91fc2b85a55f55b5f0d2d1b8c8105acb
Hapoa <liuyi2293@outlook.com>
2019-03-11 21:09:20 +0800

    修改分辨率适配比例名称

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include <QApplication>
 #include <QScreen>
 
-float gScaledSize; // for screen resolution fit
+float gScaleSize; // for screen resolution fit
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
 
     QScreen* screen = QGuiApplication::primaryScreen();
-    gScaledSize = screen->geometry().width() * 1.0 / 1920; // 1920 is the width of my computer monitor developed this app
+    gScaleSize = screen->geometry().width() * 1.0 / 1920; // 1920 is the width of my computer monitor developed this app
 
     qApp->setStyleSheet("QWidget{font-family: 'Microsoft YaHei UI';}");
 
