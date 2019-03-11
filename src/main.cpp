f1db5372db005a012601775a3ff6a312c2bf0fdc
Hapoa <liuyi2293@outlook.com>
2019-03-11 19:24:58 +0800

    添加分辨率自适应

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
-﻿#include "src/config.h"
-#include "src/main_window.h"
+﻿#include "src/main_window.h"
 
 #include <QApplication>
 #include <QScreen>
-#include <QFile>
-#include <QTextCodec>
+
+float gScaledSize; // for screen resolution fit
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
+
+    QScreen* screen = QGuiApplication::primaryScreen();
+    gScaledSize = screen->geometry().width() * 1.0 / 1920; // 1920 is the width of my computer monitor developed this app
 
     qApp->setStyleSheet("QWidget{font-family: 'Microsoft YaHei UI';}");
 
